d7b0cede2c7f6da024c8e352b1fa929e38aee0d4
fengjb <j.b.feng@foxmail.com>
2019-03-07 12:01:10 +0800

    Add files via upload

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,7 +3,8 @@ package algorithms;
 public class Main {
     public static void main(String[] args) {
         final SIMP simpOptimizer = new SIMP(args[0]);
-        final PostProcess postProcess = new PostProcess();
+        final PostProcess postProcess = new PostProcess(simpOptimizer.fem.nelx,simpOptimizer.fem.nely,
+                simpOptimizer.fem.cellModel.nelx,simpOptimizer.fem.cellModel.nely);
         Thread computeThread = new Thread(new Runnable() {
             public void run() {
                 FiniteElementAnalysis result = simpOptimizer.macroSimp(postProcess);
